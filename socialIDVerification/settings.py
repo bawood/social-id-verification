10c35aec3a57448e150f059997c41b3b4a256392
Brian Awood <bawood@umich.edu>
2017-07-25 11:40:11 -0400

    adjust some things

---
+++ socialIDVerification/settings.py
@@ -0,0 +1,93 @@
+import sys
+from decouple import config
+from unipath import Path
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = config('SECRET_KEY', default='notsecret')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = config('DEBUG', default=False, cast=bool)
+
+BASE_DIR = Path(__file__).parent.parent
+
+ALLOWED_HOSTS = ['*']
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'verify',
+)
+
+MIDDLEWARE_CLASSES=(
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+)
+
+ROOT_URLCONF='socialIDVerification.urls'
+
+TEMPLATES=[
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+)
+
+LANGUAGE_CODE='en-us'
+
+TIME_ZONE='UTC'
+
+USE_I18N=True
+
+USE_L10N=True
+
+USE_TZ=True
+
+STATIC_URL = '/static/'
+STATIC_ROOT = Path(BASE_DIR, 'static')
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+       'console': {
+           'level': DEBUG,
+           'class': 'logging.StreamHandler',
+           'stream': sys.stdout,
+       },
+     },
+     'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': config('DJANGO_LOG_LEVEL', default='INFO'),
+            'propagate': True,
+        },
+        'verify': {
+            'handlers': ['console'],
+            'level': config('LOG_LEVEL', default='INFO'),
+            'propagate': True,
+        },
+     },
+}
